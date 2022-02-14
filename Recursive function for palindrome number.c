ac9ee599a1df204d88b2ee2edd05fbb2e64e6b31
Srijani-sb <91447768+Srijani-sb@users.noreply.github.com>
2022-02-14 16:26:36 +0530

    Add files via upload

---
+++ Recursive function for palindrome number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int palin(int n,int temp)
+{
+	if (n==0)
+	{
+		return temp;
+	}
+	temp=(temp*10)+(n%10);
+	return palin(n/10,temp);
+}
+int main()
+{
+	int n,temp;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	temp=palin(n,0);
+	if(temp==n)
+	{
+		printf("Yes it is a palindrome");
+	}
+	else
+	{
+		printf("No its not a palindrome");
+	}
+	return 0;
+}
