854942ef6e611780636924e4ac8046c205e29df2
Srijani-sb <91447768+Srijani-sb@users.noreply.github.com>
2022-02-14 17:07:51 +0530

    Add files via upload

---
+++ Min and max of data types.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	long int min = (pow(2,sizeof(long int)) / 2) * -1;
+	long int max = (pow(2,sizeof(long int)) / 2) - (-1);
+	printf("%lu %lu",min,max);
+	
+	short int shrtmin = (pow(2,sizeof(short int)) / 2) * -1;
+	short int shrtmax = (pow(2,sizeof(short int)) / 2) - (-1);
+	printf("\n%hd %hd",shrtmin,shrtmax);
+	
+	int dmin = (pow(2,sizeof(int)) / 2) * -1;
+	int dmax = (pow(2,sizeof(int)) / 2) - (-1);
+	printf("\n%d %d",dmin,dmax);
+	
+	
+	return 0;
+}
